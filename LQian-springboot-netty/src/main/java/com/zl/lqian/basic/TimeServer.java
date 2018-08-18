68c0a8a8fd5530dfb25fc39083050ebcc632d1cb
zhanglei <1198902364@qq.com>
2018-08-19 00:33:25 +0800

    增加netty学习第一节

---
+++ LQian-springboot-netty/src/main/java/com/zl/lqian/basic/TimeServer.java
@@ -0,0 +1,56 @@
+package com.zl.lqian.basic;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class TimeServer {
+
+
+
+    public void bind(int port) throws Exception {
+
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 1024)
+                    .childHandler(new ChannelInitializer<SocketChannel>(){
+                        @Override
+                        protected void initChannel(SocketChannel arg0) throws Exception {
+                            arg0.pipeline().addLast(new TimeServerHandler());
+                        }
+
+                    });
+            // 绑定端口，同步等待成功
+            ChannelFuture f = b.bind(port).sync();
+
+            // 等待服务端监听端口关闭
+            f.channel().closeFuture().sync();
+
+        }finally {
+            // 优雅退出，释放线程池资源
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        int port = 8080;
+        if (args != null && args.length > 0) {
+            try {
+                port = Integer.valueOf(args[0]);
+            } catch (NumberFormatException e) {
+                // 采用默认值
+            }
+        }
+        new TimeServer().bind(port);
+    }
+}
