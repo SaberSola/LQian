bf83d1db5e9dc1fa60a89e87753bc0e5f809fdd5
zhanglei <1198902364@qq.com>
2018-09-03 21:57:10 +0800

    增加登录接口

--- LQian-springboot-sso/src/main/java/com/zl/lqian/controller/LoginController.java
+++ LQian-springboot-sso/src/main/java/com/zl/lqian/controller/LoginController.java
@@ -1,8 +1,49 @@
 package com.zl.lqian.controller;
 
 
-import org.springframework.web.bind.annotation.RestController;
+import com.alibaba.fastjson.JSONObject;
+import com.zl.lqian.entity.ResponseDTO;
+import com.zl.lqian.service.client.ApolloOAuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
 
 @RestController("/api/v1")
 public class LoginController {
+
+
+    @Value("${security.oauth2.client.client-id}")
+    private String clientId;
+    @Value("${security.oauth2.client.client-secret}")
+    private String clientSecret;
+    @Value("${security.oauth2.client.grant-type}")
+    private String grantType;
+
+    @Autowired
+    ApolloOAuthService apolloOAuthService;
+
+    @RequestMapping(value = "/loginSystem", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseDTO login(@RequestBody JSONObject jsonObject) {
+        String userName = jsonObject.getString("username");
+        String password = jsonObject.getString("password");
+        ResponseDTO resultDto = new ResponseDTO();
+        //    logger.info("-------------->登录系统操作开始");
+        try {
+
+            JSONObject responseJson = apolloOAuthService.getOAuthToken(grantType, userName, password, clientId, clientSecret, "");
+
+            String accessToken = responseJson.getString("access_token");
+
+            if (accessToken != null && accessToken.length() > 0) {
+                resultDto.setResult(0);
+                resultDto.setMessage("登录成功");
+                resultDto.setData(accessToken);
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return resultDto;
+    }
 }
---
+++ LQian-springboot-sso/src/main/java/com/zl/lqian/entity/ResponseDTO.java
@@ -0,0 +1,36 @@
+package com.zl.lqian.entity;
+
+import java.io.Serializable;
+
+public class ResponseDTO<T> implements Serializable{
+
+    private T data;
+
+    private int result;
+
+    private String message;
+
+    public int getResult() {
+        return result;
+    }
+
+    public void setResult(int result) {
+        this.result = result;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
