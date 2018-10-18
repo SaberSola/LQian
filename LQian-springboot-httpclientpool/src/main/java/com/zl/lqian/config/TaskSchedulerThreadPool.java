d9de8d82d5506037755a2a163e31f598c7794c19
zhanglei <1198902364@qq.com>
2018-10-18 22:06:27 +0800

    增加定时线程池

---
+++ LQian-springboot-httpclientpool/src/main/java/com/zl/lqian/config/TaskSchedulerThreadPool.java
@@ -0,0 +1,19 @@
+package com.zl.lqian.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+@Configuration
+public class TaskSchedulerThreadPool {
+
+    @Bean
+    public TaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setThreadNamePrefix("poolScheduler");
+        scheduler.setPoolSize(50);
+        return scheduler;
+    }
+}
