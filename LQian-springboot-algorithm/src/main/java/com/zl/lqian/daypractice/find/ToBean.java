304fb04f69ea1c72b134ca447fa48f0da93e4b57
zhanglei06469 <1198902364@qq.com>
2019-09-11 22:11:56 +0800

    增加bull

--- LQian-springboot-algorithm/src/main/java/com/zl/lqian/daypractice/find/ToBean.java
+++ LQian-springboot-algorithm/src/main/java/com/zl/lqian/daypractice/find/ToBean.java
@@ -5,6 +5,7 @@ package com.zl.lqian.daypractice.find;
 
 import com.hotels.beans.BeanUtils;
 import com.hotels.transformer.annotation.ConstructorArg;
+import lombok.Data;
 
 import java.util.List;
 
@@ -13,52 +14,26 @@ import java.util.List;
  * @Date 2019-09-10
  * @Des ${todo}
  */
+@Data
 public class ToBean {
-    private  String differentName;
-    private  int id;
-    private List<String> list;
+    final String differentName;
+    final int id;
+    final List<String> list;
 
 
 
     public ToBean(@ConstructorArg("name") final String differentName,
-                  @ConstructorArg("id")final Integer id,
-                  @ConstructorArg("list")final List list){
+                  @ConstructorArg("id")   final Integer id,
+                  @ConstructorArg("list") final List list){
         this.differentName = differentName;
         this.id = id;
         this.list = list;
     }
-
-    public ToBean(){}
     public static void main(String [] args){
         BeanUtils beanUtils = new BeanUtils();
         FromBean fromBean = new FromBean();
         fromBean.setId(1);
         fromBean.setName("zhanglei");
         ToBean toBean = beanUtils.getTransformer().transform(fromBean, ToBean.class);
-    }
-
-
-    public String getDifferentName() {
-        return differentName;
-    }
-
-    public void setDifferentName(String differentName) {
-        this.differentName = differentName;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public List<String> getList() {
-        return list;
-    }
-
-    public void setList(List<String> list) {
-        this.list = list;
     }
 }
